527c159f7e29370c014fe7ffe31daa5a95a88b78
shukuravazov <shukuravazov7@gmail.com>
2022-12-11 19:52:43 +0100

    NewReminders component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import NewReminder from "./components/NewReminder";
 import ReminderList from "./components/ReminderList";
 import Reminder from "./models/reminder";
 import reminderService from "./services/reminder";
@@ -20,8 +21,14 @@ function App() {
     setReminders(reminders.filter((reminder) => reminder.id !== id));
   };
 
+  const addReminder = async (title: string) => {
+    const newReminder = await reminderService.addReminder(title);
+    setReminders([newReminder, ...reminders]);
+  };
+
   return (
     <div className="App">
+      <NewReminder onAddReminder={addReminder} />
       <ReminderList items={reminders} onRemoveReminder={removeReminder} />
     </div>
   );
